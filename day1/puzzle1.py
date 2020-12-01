0fe1535aa7ef21f240b94d0e6b47febe288ca180
smudger <15144861+smudger@users.noreply.github.com>
2020-12-01 20:37:25 +0000

    refactor python puzzle 1 to be more performant

--- day1/puzzle1.py
+++ day1/puzzle1.py
@@ -4,8 +4,14 @@ import csv
 
 with open('expense_report.csv', newline='') as csvFile:
     expenseReport = [int(row[0]) for row in csv.reader(csvFile)]
-    for row in expenseReport:
-        hasPair = [i for i in expenseReport if i == (2020 - row)]
-        if hasPair:
-            print(row * (2020 - row))
-            break
+
+    def find_match(row):
+        return [(i, row) for i in expenseReport if i == (2020 - row)]
+
+    def parse_pair(pair):
+        return (pair[0][0], pair[0][1])
+
+    pairs = (find_match(row) for row in expenseReport)
+    matchingPair = [parse_pair(pair) for pair in pairs if pair][0]
+
+    print(matchingPair[0] * matchingPair[1])
