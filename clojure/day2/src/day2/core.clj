f107bb2c1805d4021319c3db2718d0728c4b4520
smudger <15144861+smudger@users.noreply.github.com>
2020-12-02 21:19:19 +0000

    solve puzzle1 in clojure with test data

--- clojure/day2/src/day2/core.clj
+++ clojure/day2/src/day2/core.clj
@@ -1,4 +1,28 @@
 (ns day2.core)
 
 (defn -main [& args]
-  (println "Hello, World!"))
+  (defn parse [row]
+     (def numbers (-> row
+         (get 0)
+         (clojure.string/split #"-")
+     ))
+     (list (read-string (get numbers 0)) (read-string (get numbers 1)) (subs (get row 1) 0 1) (get row 2))
+  )
+
+  (defn countMatches [row]
+     (list (nth row 0) (nth row 1) (count (re-seq (re-pattern (nth row 2)) (nth row 3))))
+  )
+
+  (defn isValid [row]
+     (and (<= (nth row 0) (nth row 2)) (>= (nth row 1) (nth row 2)))
+  )
+
+  (->> '("1-9 x: xwjgxtmrzxzmkx", "4-6 r: rrrkrgr")
+       (map #(clojure.string/split % #" "))
+       (map parse)
+       (map countMatches)
+       (filter isValid)
+       (count)
+       (println)
+  )
+)
