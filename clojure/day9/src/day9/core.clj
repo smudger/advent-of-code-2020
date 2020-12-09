278c545ef2414ac44969ddc03b169e742c4b4de4
smudger <15144861+smudger@users.noreply.github.com>
2020-12-09 20:46:26 +0000

    complete puzzle 2 in clojure

--- clojure/day9/src/day9/core.clj
+++ clojure/day9/src/day9/core.clj
@@ -20,13 +20,41 @@
     )
   )
 
-  (->>
+  (def weakness (->>
     (drop 25 nums)
     (map #(vector (last %) (map last (drop-last (- (count nums) (first %)) (drop (- (first %) 25) nums)))))
     (into (sorted-map))
     (remove #(apply hasSum %))
     (first)
     (first)
-    (println)
+    (read-string)
+  ))
+
+  (defn findSum [sum]
+    (defn addNum [acc num]
+      (def result (+ (reduce + acc) (read-string (last num))))
+      (if (< result sum)
+         (cons (read-string (last num)) acc)
+         (reduced (cons (read-string (last num)) acc))
+      )
+    )
+
+    (defn countUp [startIdx] (
+      ->> nums
+          (drop startIdx)
+          (reduce addNum [])
+    ))
+
+    (->>
+      (map #(vector (last %) (countUp (first %))) nums)
+      (filter #(> (count (last %)) 1))
+      (filter #(= sum (reduce + (last %))))
+      (map last)
+      (map sort)
+      (map #(+ (first %) (last %)))
+      (first)
+    )
   )
+
+  (println (findSum weakness))
 )
