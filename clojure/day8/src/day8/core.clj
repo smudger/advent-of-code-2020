990aa584c038f97addf4efcdb6c1b3993b909678
smudger <15144861+smudger@users.noreply.github.com>
2020-12-09 10:21:35 +0000

    refactor puzzle 1

--- clojure/day8/src/day8/core.clj
+++ clojure/day8/src/day8/core.clj
@@ -1,12 +1,26 @@
 (ns day8.core (:gen-class :main true))
 
 (defn -main [& args]
-  (defn call [func & args] (
-    -> func
-       symbol
-       resolve
-       (apply args)
+  (defn nextIndex [op] (
+    if (= "jmp" (first op))
+    (+ (last op) (read-string (nth op 1)))
+    (+ (last op) 1)
   ))
+
+  (defn runSeq [ops]
+    (defn parseOp [op seenOps]
+      (if (some #(= op %) seenOps)
+        seenOps
+        (->> seenOps
+             (cons op)
+             (parseOp (nth ops (nextIndex op)))
+             reverse
+        )
+      )
+    )
+
+    (parseOp (first ops) [])
+  )
 
   (def ops (->>
      (->
@@ -17,40 +31,14 @@
     (map-indexed (fn [idx itm] (conj itm idx)))
   ))
 
-  (defn jmp [chg cur] (->>
-    (read-string chg)
-    (+ cur)
-    (nth ops)
-  ))
-
-  (defn acc [chg cur] (->>
-    (+ cur 1)
-    (nth ops)
-  ))
-
-  (defn nop [chg cur] (->>
-    (+ cur 1)
-    (nth ops)
-  ))
-
-  (defn sortByExecution [op seenOps]
-    (if (some #(= op %) seenOps)
-      seenOps
-      (->> seenOps
-           (cons op)
-           (sortByExecution (apply call op))
-           reverse
-      )
-    )
-  )
-
   (->>
-    (sortByExecution (first ops) ())
+    (runSeq ops)
     (filter #(= "acc" (first %)))
     (reduce (fn [acc op] (->>
       (second op)
       (read-string)
       (+ acc)
     )) 0)
+    (println)
   )
 )
