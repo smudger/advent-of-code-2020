7f97706b89a923d0c1d93529f3297a6fc86d96a5
smudger <15144861+smudger@users.noreply.github.com>
2020-12-18 21:43:16 +0000

    complete puzzle 2

--- clojure/day18/src/day18/core.clj
+++ clojure/day18/src/day18/core.clj
@@ -2,10 +2,10 @@
 
 (def ops {"+" + "*" *})
 
-(defn parseChar [char] 
+(defn parseChar [char]
   (if (re-find #"\d" char)
       (read-string char)
-      (get ops char) 
+      (get ops char)
   ))
 
 (defn evaluateSimple [expr] (let [[start & rest] (->>
@@ -18,20 +18,27 @@
     )
 ))
 
+(defn evaluateAdditionFirst [expr] (->>
+  (clojure.string/split expr #" \* ")
+  (map (comp read-string evaluateSimple))
+  (reduce *)
+  (str)
+))
+
 (defn evaluateBrackets [expr] (->>
   (re-seq #"\(([^\(\)]*)\)" expr)
-  (map #(list (first %) (evaluateSimple (last %))))
+  (map #(list (first %) (evaluateAdditionFirst (last %))))
   (reduce (fn [expr [from to]] (clojure.string/replace expr from to)) expr)
   ))
 
-(defn evaluateExpression [expr] 
+(defn evaluateExpression [expr]
   (if (re-find #"\(([^\(\)]*)\)" expr)
       (recur (evaluateBrackets expr))
-      ((comp read-string evaluateSimple) expr)
+      ((comp read-string evaluateAdditionFirst) expr)
   )
 )
 
-(defn -main [& args] (->> 
+(defn -main [& args] (->>
   (->
     (slurp "resources/expressions.txt")
     (clojure.string/split #"\n")
