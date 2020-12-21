b8c86bfd2eefa6ce6bbf3e0603fefafb975c245d
smudger <15144861+smudger@users.noreply.github.com>
2020-12-21 17:19:22 +0000

    complete puzzle 1

--- clojure/day21/spec/day21/core_spec.clj
+++ clojure/day21/spec/day21/core_spec.clj
@@ -22,6 +22,17 @@
   )
 )
 
+(describe "The ingredients getter"
+  (it "should return a unique set of ingredients from the menu"
+    (should= #{"abc" "def" "ghi" "jkl" "noj" "awdwa" "adwa"} (get-ingredients [
+      {:ingredients ["abc" "def", "abc"], :allergens ["fish" "egg" "nuts"]}
+      {:ingredients ["ghi" "jkl"], :allergens ["nuts" "sesame" "egg"]}
+      {:ingredients ["noj" "awdwa"], :allergens ["sesame" "sesame" "egg"]}
+      {:ingredients ["ghi" "adwa"], :allergens ["sesame" "nuts" "nuts"]}
+    ]))
+  )
+)
+
 (describe "The possible ingredient finder"
   (it "should return a set of the ingredients always present with the allergen"
     (should= #{"abc" "def"} (get-possible-ingredients [
@@ -30,5 +41,55 @@
       {:ingredients ["ghi" "bdh" "def"], :allergens ["sesame" "nuts"]}
       {:ingredients ["def" "abc" "bdh" "dwa"], :allergens ["sesame" "egg" "fish"]}
     ] "egg"))
+  )
+)
+
+(describe "The menu translator"
+  (it "should return a map of translations from ingredients to their allergens"
+    (should= {"nuts" "def", "egg" "abc", "sesame" "ghi", "soy" "fod"} (translate-menu [
+      {:ingredients ["abc" "def" "ghi"], :allergens ["egg"]}
+      {:ingredients ["def" "abc"], :allergens ["nuts" "egg"]}
+      {:ingredients ["ghi" "def"], :allergens ["sesame" "nuts"]}
+      {:ingredients ["def" "abc" "bdh" "dwa"], :allergens ["egg"]}
+      {:ingredients ["abc" "def" "fod"], :allergens ["soy"]}
+    ]))
+  )
+  (it "should remove taken allergens as they are assigned"
+    (should= {"dairy" "mxmxvkd", "fish" "sqjhc", "soy" "fvjkl"} (translate-menu [
+      {:ingredients ["mxmxvkd" "kfcds" "sqjhc" "nhms"], :allergens ["dairy" "fish"]}
+      {:ingredients ["trh" "fvjkl" "sbzzf" "mxmxvkd"], :allergens ["dairy"]} 
+      {:ingredients ["sqjhc" "fvjkl"], :allergens ["soy"]}
+      {:ingredients ["sqjhc" "mxmxvkd" "sbzzf"], :allergens ["fish"]}
+    ]))
+  )
+)
+
+(describe "The safe ingredient finder"
+  (it "should find the ingredients in the menu that are not allergenic"
+    (should= #{"bdh" "fgi" "dwa" "xyz"} (find-safe-ingredients [
+      {:ingredients ["abc" "def" "ghi" "fgi"], :allergens ["egg"]}
+      {:ingredients ["def" "abc"], :allergens ["nuts" "egg"]}
+      {:ingredients ["ghi" "def"], :allergens ["sesame" "nuts"]}
+      {:ingredients ["def" "abc" "bdh" "dwa" "xyz"], :allergens ["egg"]}
+      {:ingredients ["abc" "def" "fod"], :allergens ["soy"]}
+    ]))
+  )
+)
+
+(describe "The ingredient counter"
+  (it "should count the number of times an ingredient appears in the menu"
+    (should= 3 (count-ingredient [
+      {:ingredients ["abc" "def" "ghi" "fgi"], :allergens ["egg"]}
+      {:ingredients ["def" "abc"], :allergens ["nuts" "egg"]}
+      {:ingredients ["ghi" "def"], :allergens ["sesame" "nuts"]}
+      {:ingredients ["def" "bdh" "dwa" "xyz"], :allergens ["egg"]}
+      {:ingredients ["abc" "def" "fod"], :allergens ["soy"]}
+    ] "abc"))
+  )
+)
+
+(describe "The puzzle solvers"
+  (it "should solve puzzle 1"
+    (should= 5 (solve-1 "mxmxvkd kfcds sqjhc nhms (contains dairy, fish)\ntrh fvjkl sbzzf mxmxvkd (contains dairy)\nsqjhc fvjkl (contains soy)\nsqjhc mxmxvkd sbzzf (contains fish)"))
   )
 )
--- clojure/day21/src/day21/core.clj
+++ clojure/day21/src/day21/core.clj
@@ -1,7 +1,8 @@
-(ns day21.core)
+(ns day21.core
+ (:require [clojure.set]))
 
 (defn parse-menu [menu] (->>
-  (-> 
+  (->
     menu
     (clojure.string/replace #",|\(|\)" "")
     (clojure.string/split-lines)
@@ -18,6 +19,12 @@
   (reduce into #{})
 ))
 
+(defn get-ingredients [menu] (->>
+  menu
+  (map :ingredients)
+  (reduce into #{})
+))
+
 (defn get-possible-ingredients [menu allergen] (->>
   menu
   (filter (fn [dish] (some #(= allergen %) (:allergens dish))))
@@ -26,6 +33,68 @@
   (reduce clojure.set/intersection)
 ))
 
+(defn remove-used [mappings mapping] (let [correct (->>
+  mappings
+  (vals)
+  (reduce into #{})
+  (clojure.set/difference (val mapping))
+)] (hash-map (key mapping) correct)
+))
+
+(defn find-correct
+  ([possibilities] (find-correct {} possibilities))
+  ([mappings possibilities]
+    (if (empty? possibilities)
+        mappings
+        (let [sorted (->>
+          possibilities
+          (map #(remove-used mappings %))
+          (reduce into {})
+          (sort-by #(count (val %)))
+        )]
+          (recur (assoc mappings (key (first sorted)) (val (first sorted))) (rest sorted))
+        )
+    )
+  )
+)
+
+(defn translate-menu [menu] (let [possibilities (->>
+  menu
+  (get-allergens)
+  (mapv #(hash-map % (get-possible-ingredients menu %)))
+  (reduce into {})
+  )] (->>
+      possibilities
+      (find-correct)
+      (reduce-kv #(assoc %1 %2 (first %3)) {})
+  )
+))
+
+(defn find-safe-ingredients [menu] (->>
+  menu
+  (translate-menu)
+  (vals)
+  (set)
+  (clojure.set/difference (get-ingredients menu))
+))
+
+(defn count-ingredient [menu ingredient] (let [counts (->>
+  menu
+  (map :ingredients)
+  (reduce into [])
+  (frequencies)
+)] (get counts ingredient)
+))
+
+(defn solve-1 [input] (let [menu (parse-menu input)]
+  (->>
+    (find-safe-ingredients menu)
+    (map #(count-ingredient menu %))
+    (reduce +)
+  )
+))
+
 (defn -main
   [& args]
-  (println "Hello World"))
+  (println (solve-1 (slurp "resources/menu.txt")))
+)
