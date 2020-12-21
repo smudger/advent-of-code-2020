f463fe6eeba71169f335a1723faf471f370c7dbc
smudger <15144861+smudger@users.noreply.github.com>
2020-12-21 14:31:35 +0000

    add allergen tools

--- clojure/day21/spec/day21/core_spec.clj
+++ clojure/day21/spec/day21/core_spec.clj
@@ -11,3 +11,24 @@
   )
 )
 
+(describe "The allergens getter"
+  (it "should return a unique set of allergens from the menu"
+    (should= #{"fish" "egg" "nuts" "sesame"} (get-allergens [
+      {:ingredients ["abc" "def"], :allergens ["fish" "egg" "nuts"]}
+      {:ingredients ["ghi" "jkl"], :allergens ["nuts" "sesame" "egg"]}
+      {:ingredients ["noj" "awdwa"], :allergens ["sesame" "sesame" "egg"]}
+      {:ingredients ["ghi" "adwa"], :allergens ["sesame" "nuts" "nuts"]}
+    ]))
+  )
+)
+
+(describe "The possible ingredient finder"
+  (it "should return a set of the ingredients always present with the allergen"
+    (should= #{"abc" "def"} (get-possible-ingredients [
+      {:ingredients ["abc" "def" "ghi"], :allergens ["fish" "egg" "nuts"]}
+      {:ingredients ["ghi" "def" "abc"], :allergens ["nuts" "sesame" "egg"]}
+      {:ingredients ["ghi" "bdh" "def"], :allergens ["sesame" "nuts"]}
+      {:ingredients ["def" "abc" "bdh" "dwa"], :allergens ["sesame" "egg" "fish"]}
+    ] "egg"))
+  )
+)
--- clojure/day21/src/day21/core.clj
+++ clojure/day21/src/day21/core.clj
@@ -12,6 +12,20 @@
   (mapv #(zipmap [:ingredients :allergens] %))
 ))
 
+(defn get-allergens [menu] (->>
+  menu
+  (map :allergens)
+  (reduce into #{})
+))
+
+(defn get-possible-ingredients [menu allergen] (->>
+  menu
+  (filter (fn [dish] (some #(= allergen %) (:allergens dish))))
+  (map :ingredients)
+  (map set)
+  (reduce clojure.set/intersection)
+))
+
 (defn -main
   [& args]
   (println "Hello World"))
